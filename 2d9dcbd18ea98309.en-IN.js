57914a7a5bba6bb0fa6686ff6c8feb8a0a1d8404
Ankit Pramanik <ankitpramanik849@gmail.com>
2025-02-13 18:50:41 +0530

    Add files via upload

---
+++ 2d9dcbd18ea98309.en-IN.js
@@ -0,0 +1,11 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["en-IN"] = Object.assign(window['cmsg']['assets']["en-IN"] || {}, {
+    "15706": {
+        "js": ["66cf6f5530e7b4c4.strings.js"],
+        "css": []
+    },
+    "38701": {
+        "js": ["937bad62edd793b4.strings.js"],
+        "css": []
+    }
+});
